aa44a4f0a3cdef5df796cdfbfab5234cfefa93eb
mohamed nagi <mohamed_nagi_93@hotmail.com>
2018-03-29 12:29:09 +0200

    Added Files

---
+++ TimerApp/MyCustomButton.swift
@@ -0,0 +1,21 @@
+//
+//  MyCustomButton.swift
+//  TimerApp
+//
+//  Created by Sierra on 2/21/18.
+//  Copyright © 2018 Nagiz. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
